669c2ac582833db663eef7dce9480e29ddfa4efb
Santthosh <santthosh@gmail.com>
2016-03-30 05:43:49 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # aws-es-kibana
-AWS ElasticSearch Kibana Proxy
+
+AWS ElasticSearch/Kibana Proxy to access your [AWS ES](https://aws.amazon.com/elasticsearch-service/) cluster. This is the solution for accessing your cluster if you have (configured access policies)[http://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-createupdatedomains.html#es-createdomain-configure-access-policies] for your ES domain
+
+## Usage
+
+Install the npm module 
+
+`npm install -g aws-es-kibana`
+
+Set AWS credentials
+                          
+`export AWS_ACCESS_KEY_ID=XXXXXXXXXXXXXXXXXXX`
+`export AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXXXXXX`
+
+Run the proxy
+
+`aws-es-kibana <cluster-endpoint>`
+
+## Credits
+
+Adopted from this [gist](https://gist.github.com/nakedible-p/ad95dfb1c16e75af1ad5). Thanks @nakedible-p
+
---
+++ index.js
@@ -0,0 +1,76 @@
+var AWS = require('aws-sdk');
+var http = require('http');
+var httpProxy = require('http-proxy');
+var express = require('express');
+var bodyParser = require('body-parser');
+var stream = require('stream');
+
+if (process.argv.length != 3) {
+    console.error('usage: aws-es-proxy <aws-es-cluster-endpoint>');
+    process.exit(1);
+}
+var ENDPOINT = process.argv[2];
+var m = ENDPOINT.match(/\.([^.]+)\.es\.amazonaws\.com\.?$/);
+if (!m) {
+    console.error('region cannot be parsed from endpoint address, must end in .<region>.es.amazonaws.com');
+    process.exit(1);
+}
+var REGION = m[1];
+var TARGET = 'https://' + process.argv[2];
+var PORT = 9200;
+var BIND_ADDRESS = '127.0.0.1';
+
+var creds;
+var chain = new AWS.CredentialProviderChain();
+chain.resolve(function (err, resolved) {
+    if (err) throw err;
+    else creds = resolved;
+});
+
+function getcreds(req, res, next) {
+    return creds.get(function (err) {
+        if (err) return next(err);
+        else return next();
+    });
+}
+var proxy = httpProxy.createProxyServer({
+    target: TARGET,
+    changeOrigin: true,
+    secure: true
+});
+
+var app = express();
+app.use(bodyParser.raw({type: '*/*'}));
+app.use(getcreds);
+app.use(function (req, res) {
+    var bufferStream;
+    if (Buffer.isBuffer(req.body)) {
+        var bufferStream = new stream.PassThrough();
+        bufferStream.end(req.body);
+    }
+    proxy.web(req, res, {buffer: bufferStream});
+});
+
+proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    var endpoint = new AWS.Endpoint(ENDPOINT);
+    var request = new AWS.HttpRequest(endpoint);
+    request.method = proxyReq.method;
+    request.path = proxyReq.path;
+    request.region = REGION;
+    if (Buffer.isBuffer(req.body)) request.body = req.body;
+    if (!request.headers) request.headers = {};
+    request.headers['presigned-expires'] = false;
+    request.headers['Host'] = ENDPOINT;
+
+    var signer = new AWS.Signers.V4(request, 'es');
+    signer.addAuthorization(creds, new Date());
+
+    proxyReq.setHeader('Host', request.headers['Host']);
+    proxyReq.setHeader('X-Amz-Date', request.headers['X-Amz-Date']);
+    proxyReq.setHeader('Authorization', request.headers['Authorization']);
+    if (request.headers['x-amz-security-token']) proxyReq.setHeader('x-amz-security-token', request.headers['x-amz-security-token']);
+});
+
+http.createServer(app).listen(PORT, BIND_ADDRESS);
+console.log('AWS ES cluster available at http://' + BIND_ADDRESS + ':' + PORT);
+console.log('Kibana available at http://' + BIND_ADDRESS + ':' + PORT + '/_plugin/kibana/');
